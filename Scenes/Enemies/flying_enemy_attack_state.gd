08993cce2a4b4f4cb7318ff29692128218389241
Yamen <77121917+Yamen-M@users.noreply.github.com>
2024-08-04 21:41:41 +0300

    Added Shader. Fixed code to turn White on damage. re-uploaded flying_enemy scene for safety.

--- Scenes/Enemies/flying_enemy_attack_state.gd
+++ Scenes/Enemies/flying_enemy_attack_state.gd
@@ -1,6 +1,7 @@
 extends State
 class_name FlyingEnemyAttack
 
+@onready var sprite : Sprite2D = $"../../Sprite2D"
 @onready var animation_player = $"../../AnimationPlayer"
 @export var enemy :CharacterBody2D
 var player:CharacterBody2D
@@ -11,6 +12,11 @@ func state_enter():
 		
 func to_idle():
 	transitioned.emit(self, "idle")
-	
+
+func SetShader_BlinkIntensity(newValue : float):
+	sprite.material.set_shader_parameter("blink_intensity", newValue)
+
+
 func _on_hurt_box_component_being_hit():
-	enemy.sprite.modulate = Color.WHITE # NOT WORKING
+	var tween = get_tree().create_tween()
+	tween.tween_method(SetShader_BlinkIntensity, 1.0, 0.0, 0.5)
