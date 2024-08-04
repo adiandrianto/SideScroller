a19b5a701b0f58654afdc4b722eee98368b718f4
Yamen <77121917+Yamen-M@users.noreply.github.com>
2024-08-04 13:11:07 +0300

    Added "up" to enter door, "down" to exit door. Changed from collide with area to is colliding() so that it only activates when only the player is on door.

--- Scripts/Component/door.gd
+++ Scripts/Component/door.gd
@@ -2,18 +2,27 @@ extends Node2D
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayera
 @onready var tile_map: TileMap = $"../TileMap"
 @onready var second_dimension: TileMapLayer = $"../GroundTile/2ndDimension"
 @onready var ray_cast_2d: RayCast2D = $RayCast2D
+@onready var player: CharacterBody2D = $"../Player"
 
 func _process(delta: float) -> void:
-	if ray_cast_2d.collide_with_areas:
+	if ray_cast_2d.is_colliding():
 		print(ray_cast_2d.get_collider())
+		if Input.is_action_pressed("up"):
+			open()
+		if Input.is_action_pressed("down"):
+			close()
 		
 func open():
 	animation_player.play("open")
 	await animation_player.animation_finished
 	tile_map.visible = false
 	second_dimension.collision_enabled = true
+
+func close():
+	second_dimension.collision_enabled = false
+	tile_map.visible = true
 	
