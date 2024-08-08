f579518392c3de27ee4307f790bd657307d280b1
Yamen <77121917+Yamen-M@users.noreply.github.com>
2024-08-08 21:58:40 +0300

    Added BloddSplatter to enemy a death scene and cleaned code.

--- Scenes/Enemies/enemy_A_death_scene.gd
+++ Scenes/Enemies/enemy_A_death_scene.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var blood_splatter: AudioStreamPlayer = $BloodSplatter
 
 var last_pitch = 1.0
 
 func _ready() -> void:
 	random_pitch()
+	random_pitch2()
 	animation_player.play("death")
 
 func random_pitch(from_position=0.0):
@@ -20,4 +22,16 @@ func random_pitch(from_position=0.0):
 	last_pitch = audio_stream_player.pitch_scale
 	
 	audio_stream_player.play(0.0)
+
+func random_pitch2(from_position=0.0):
+	randomize()
+	blood_splatter.pitch_scale = randf_range(1.2, 1.5)
+	
+	while abs(blood_splatter.pitch_scale - last_pitch) < .1:
+		randomize()
+		blood_splatter.pitch_scale = randf_range(0.8, 1.2)
+	
+	last_pitch = blood_splatter.pitch_scale
+	
+	blood_splatter.play(0.0)
 	
--- Scenes/Enemies/enemy_a.gd
+++ Scenes/Enemies/enemy_a.gd
@@ -11,8 +11,6 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var state_machine: Node = $StateMachine
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var ray_cast: RayCast2D = $RayCast2D
-@onready var dead_sfx: AudioStreamPlayer2D = $DeadSFX
-@onready var blood_splatter: AudioStreamPlayer2D = $BloodSplatter
 const ENEMY_A_DEATH_SCENE = preload("res://Scenes/Enemies/enemy_a_death_scene.tscn")
 
 func _physics_process(delta: float) -> void:
@@ -25,33 +23,15 @@ func _process(delta: float) -> void:
 	state_label.text = str(health_component.current_health)
 	
 func _on_health_component_died() -> void:
-	#var deadsfx = audio_stream_player.instantiate()
-	#get_tree().root.add_child(deadsfx)
-	
+
 	var death_scene = ENEMY_A_DEATH_SCENE.instantiate()
 	death_scene.global_position = global_position
 	get_tree().root.add_child(death_scene)
-	bloodSFX()
-	
 	queue_free()
 
 func SetShader_BlinkIntensity(newValue : float):
 	sprite.material.set_shader_parameter("blink_intensity", newValue)
 
-func deadSFX():
-	pass
-
-func bloodSFX():
-	randomize()
-	blood_splatter.pitch_scale = randf_range(0.8, 1.2)
-	
-	while abs(blood_splatter.pitch_scale - last_pitch) < .1:
-		randomize()
-		blood_splatter.pitch_scale = randf_range(0.8, 1.2)
-	
-	last_pitch = blood_splatter.pitch_scale
-	
-	blood_splatter.play(0.0)
 
 
 func _on_health_component_health_changed() -> void:
