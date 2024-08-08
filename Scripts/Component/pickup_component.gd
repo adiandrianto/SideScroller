af4d53e1f68a5975e3f6597a60314ad47f6cb105
Adi Andrianto <adiandrianto@gmail.com>
2024-08-08 19:51:56 +0700

    add machinegun pickup sprite

---
+++ Scripts/Component/pickup_component.gd
@@ -0,0 +1,15 @@
+extends Area2D
+class_name PickUp
+
+@onready var player: CharacterBody2D
+@export var animation_player: AnimationPlayer
+@export var audio_stream_player: AudioStreamPlayer
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
+	animation_player.play("idle")
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		animation_player.play("pick_up")
+		audio_stream_player.play()
