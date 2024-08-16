3d74a76e5350898e94d493b6041b4506469f5c49
Adi Andrianto <adiandrianto@gmail.com>
2024-08-16 10:48:01 +0700

    add enemy_visible_notifier

---
+++ enemy_visible_notifier.gd
@@ -0,0 +1,7 @@
+extends VisibleOnScreenNotifier2D
+
+@onready var player: Player = get_tree().get_first_node_in_group("player")
+
+func _on_screen_exited() -> void:
+	if owner.global_position.x - player.global_position.x < -80:
+		owner.queue_free()
