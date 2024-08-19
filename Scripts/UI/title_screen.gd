d41f173a613104bfe7b4835cd4369a746bea4481
Yamen <77121917+Yamen-M@users.noreply.github.com>
2024-08-19 20:13:53 +0300

    Added click and hover sound to titlescreen

--- Scripts/UI/title_screen.gd
+++ Scripts/UI/title_screen.gd
@@ -3,26 +3,41 @@ extends Control
 @onready var start: Button = $Start
 @onready var credits: Button = $Credits
 @onready var exit: Button = $Exit
+@onready var click: AudioStreamPlayer2D = $Click
+@onready var hover: AudioStreamPlayer2D = $Hover
 
 func _ready() -> void:
 	AudioPlayer.play_theme_music()
 
 func _on_start_pressed() -> void:
+	click.play()
 	visibility()
 	AudioPlayer.stop()
 	animation_player.play("fade_out")
 	await animation_player.animation_finished
 	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
 
+func _on_start_mouse_entered() -> void:
+	hover.play()
 
 func _on_credits_pressed() -> void:
+	click.play()
 	visibility()
 	animation_player.play("fade_out")
 	await animation_player.animation_finished
 	get_tree().change_scene_to_file("res://Scenes/Credits/Credits.tscn")
 
+
+func _on_credits_mouse_entered() -> void:
+	hover.play()
+
 func _on_exit_pressed() -> void:
+	click.play()
+	await click.finished
 	get_tree().quit()
+
+func _on_exit_mouse_entered() -> void:
+	hover.play()
 
 func visibility():
 	start.visible = false
