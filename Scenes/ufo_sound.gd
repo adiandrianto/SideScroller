242db1b38c5f2494bebb7149c39ded00164d64f1
Adi Andrianto <adiandrianto@gmail.com>
2024-08-19 12:52:48 +0700

    add opening 2

---
+++ Scenes/ufo_sound.gd
@@ -0,0 +1,9 @@
+extends AudioStreamPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func play_sound():
+	play(0.0)
