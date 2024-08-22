a2c2d63b7ae65eab614758a0d125e3d059e5a8ed
Adi Andrianto <adiandrianto@gmail.com>
2024-08-22 10:28:38 +0700

    fix flying enemy still visible on 2nd dimension

--- Scripts/Enemies/flying_enemy.gd
+++ Scripts/Enemies/flying_enemy.gd
@@ -10,8 +10,28 @@ class_name FlyingEnemy
 @onready var state_machine = $StateMachine
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
+var is_inside := false
+
 func _process(delta):
 	state_label.text = state_machine.current_state.name
+	if is_inside && DimensionManager.is_inside:
+		can_be_seen()
+	elif is_inside && not DimensionManager.is_inside:
+		invincible()
+	elif not is_inside && DimensionManager.is_inside:
+		invincible()
+	else :
+		can_be_seen()
+		
+func invincible():
+	visible = false
+	hurt_box_component.monitorable = false
+	hurt_box_component.monitoring = false
+	
+func can_be_seen():
+	visible = true
+	hurt_box_component.monitorable = true
+	hurt_box_component.monitoring = true
 	
 func _physics_process(delta):
 	move_and_slide()
--- enemy_visible_notifier.gd
+++ enemy_visible_notifier.gd
@@ -3,5 +3,5 @@ extends VisibleOnScreenNotifier2D
 @onready var player: Player = get_tree().get_first_node_in_group("player")
 
 func _on_screen_exited() -> void:
-	if owner.global_position.x - player.global_position.x < -200:
+	if owner.global_position.x - player.global_position.x < -300:
 		owner.queue_free()
